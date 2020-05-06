7ba2f499ca353ac7234cf49487bb5056e8c7d317
Nick Young <nick.young@auckland.ac.nz>
2020-05-06 12:08:26 +1200

    init

---
+++ collect.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from netaddr import IPNetwork
+import sys
+import requests
+from requests.exceptions import RequestException
+from tqdm.auto import tqdm
+import mysql.connector
+import json
+import config
+
+network = list(IPNetwork(config.subnet))
+print(f"Scanning {network[0]} to {network[-1]}")
+
+db = mysql.connector.connect(
+  host="localhost",
+  user=config.user,
+  passwd=config.passwd,
+  database="quest"
+)
+cur = db.cursor()
+
+for ip in tqdm(network):
+    try:
+        r = requests.get(f"http://{ip}:8080/get-json", timeout=1)
+    except RequestException:
+        continue
+    data = r.json()
+    serial = data["sysInfo"]["serial"]
+    print(ip, serial)
+    sql = "INSERT IGNORE INTO quest (ip, serial, json) VALUES (%s, %s, %s)"
+    val = (str(ip), serial, json.dumps(data))
+    cur.execute(sql, val)
+    db.commit()
